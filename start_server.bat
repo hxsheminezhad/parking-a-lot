934b6de0dfb3777d78cc2938dec38da21122eec8
Ardeshir Hassanzadeh <87963609+hxsheminezhad@users.noreply.github.com>
2026-09-10 10:38:52 +0700

    Delete start_server.bat

--- start_server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Parking-A-Lot Real-Time Server Host
-echo ============================================================
-echo   🚗 Starting Parking-A-Lot Real-Time Server Host...
-echo ============================================================
-echo.
-cd /d "%~dp0"
-node server.js
-pause
